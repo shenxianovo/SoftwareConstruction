52ab3a1380b2615bf1a893c1d9d0daac6b1ebc82
shenxianovo <shenxianovo@outlook.com>
2025-02-21 10:11:51 +0800

    HW01 Finished

---
+++ HW01/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using HW01.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HW01
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // 控件添加到页面的控件树时触发
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "ExerciseOne_Page")
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+            ContentFrame.Navigate(typeof(ExerciseOnePage));
+        }
+
+        // 选择某个菜单项时触发
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+
+            // 奇怪，用 InvokenItem 似乎不行（）它返回的是这个 Item 的 Content
+            NavigationViewItem ItemContent = args.InvokedItemContainer as NavigationViewItem;
+            if (ItemContent == null)
+            {
+                return;
+            }
+
+            switch (ItemContent.Tag)
+            {
+                case "ExerciseOne_Page":
+                    ContentFrame.Navigate(typeof(ExerciseOnePage));
+                    break;
+                case "ExerciseTwo_Page":
+                    ContentFrame.Navigate(typeof(ExerciseTwoPage));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+        }
+
+    }
+}
---
+++ HW01/Views/ExerciseTwoPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HW01.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ExerciseTwoPage : Page
+    {
+        public ExerciseTwoPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int a = Int32.Parse(boxA.Text);
+                int b = Int32.Parse(boxB.Text);
+                int c = a;
+
+                if (a > b)
+                {
+                    text.Text = PrimeNumber.CalculateBetween(b, a);
+                }
+                else
+                {
+                    text.Text = PrimeNumber.CalculateBetween(a, b);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageDialog dialog = new MessageDialog("Invalid Parameter!");
+                await dialog.ShowAsync();
+            }
+        }
+    }
+
+    public class PrimeNumber
+    {
+        private static bool IsPrime(int n)
+        {
+            if (n <= 1)
+            {
+                throw new FormatException();
+            }
+
+            // Some optimization
+            if (n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || n == 13)
+            {
+                return true;
+            }
+
+            if (n % 2 == 0)
+            {
+                return false;
+            }
+
+            for (int i = 3; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // [a, b)
+        public static string CalculateBetween(int a, int b)
+        {
+            StringBuilder retVal = new StringBuilder();
+
+            for (int i = a; i < b; i++)
+            {
+                if (IsPrime(i))
+                {
+                    retVal.AppendLine(i.ToString());
+                }
+            }
+
+            return retVal.ToString();
+        }
+    }
+}
