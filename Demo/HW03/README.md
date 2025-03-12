fd23b851af46163c2edddccf1507cc5f6a93fd0a
shenxianovo <shenxianovo@outlook.com>
2025-03-12 09:26:30 +0800

    HW03: Basic function implemented

---
+++ Demo/HW03/README.md
@@ -0,0 +1,69 @@
+# 第三次作业
+
+## 作业内容
+
+1. 使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。在闹钟走时时或者响铃时，在控制台/Winform界面上显示提示信息
+
+## 运行效果
+
+1. 实现了走时时对界面更新的事件
+2. 实现了响铃时对界面更新的事件
+
+
+## 遇到的问题以及解决方式
+
+八股部分结束，下面是遇到的一些问题
+
+### 1. System.Runtime.InteropServices.COMException: ''
+
+``` cs
+// Model 中定义了事件
+public event EventHandler? TimeChanged;
+
+// ViewModel 中订阅了事件并封装了事件处理函数
+private void OnTimeChanged(object? sender, EventArgs e)
+{
+    // 通知 UI 更新
+    OnPropertyChanged(nameof(DefaultClockTime)); // 此处运行时报错
+}
+```
+
+在多线程环境中更新UI元素时，可能会遇到该错误。
+UI元素只能在创建它们的线程（通常是主线程）上进行更新
+而Timer的回调方法是在一个线程池线程中执行的（非 UI 线程）
+
+解决方案：
+
+使用 DispacherQueue
+``` cs
+private void OnTimeChanged(object? sender, EventArgs e)
+{
+    _dispatcherQueue.TryEnqueue(() =>
+    {
+        // 通知 UI 更新
+        OnPropertyChanged(nameof(DefaultClockTime));
+    });
+}
+```
+
+### 2. CsWinRT Error CS0227: Unsafe code may only appear if compiling with /unsafe`
+
+将下面的 `ObservableCollections` 字段更改为属性后出现上述报错。（更改为属性的原因是需要在 UI 中进行绑定）
+``` cs
+public ObservableCollection<TimeOnly> AlarmItems;
+public ObservableCollection<TimeOnly> AlarmItems { get; set; } // 更改为属性后报错
+```
+
+STFW 后获取了一些资料:
+- [一些资料](https://www.reddit.com/r/csharp/comments/1gpvdef/i_had_a_winui_3_application_using_c_12_and_net_8/)
+
+开发者原话：
+```
+This is due to some generated code to marshal generic collections requiring unsafe blocks. You're getting that error because you're on an older version of CsWinRT. We've added a new analyzer for this to provide a better experience in the new release (2.2.1) 
+```
+
+2.2.0 版本的 `CsWinRt` 需要使用 `unsafe` 来进行代码生成。新版 (2.2.1) 会解决这个问题 ~~但是目前还没发布...~~
+
+解决方案：
+
+在项目配置中启用 `unsafe block`
--- HW03/Clock.cs
+++ HW03/Clock.cs
@@ -2,8 +2,10 @@
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Content;
 using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Runtime.ExceptionServices;
@@ -18,28 +20,43 @@ namespace HW03
         private Timer _timer;
 
         public event EventHandler? TimeChanged;
+        public event EventHandler? AlarmRaised;
+        public event EventHandler? AlarmItemsChanged;
 
         public TimeOnly ClockTime
         {
             get => field;
-            set 
+            set
             {
                 if (field != value)
                 {
                     field = value;
                     TimeChanged?.Invoke(this, EventArgs.Empty);
                 }
-            } 
+                if (AlarmItems.Contains(field))
+                {
+                    AlarmRaised?.Invoke(this, EventArgs.Empty);
+                }
+            }
         }
 
-        public List<TimeOnly> AlarmTimes { get; set; }
+        public List<TimeOnly> AlarmItems { get; set; }
 
         public Clock()
         {
+            AlarmItems = new List<TimeOnly>();
             ClockTime = new TimeOnly(23, 33, 33);
-            AlarmTimes = new List<TimeOnly>();
 
-            _timer = new Timer((state)=>Tick(), null, 0, 1000);
+            _timer = new Timer((state) => Tick(), null, 0, 1000);
+        }
+
+        public void AddAlarm(TimeOnly alarm)
+        {
+            if (!AlarmItems.Contains(alarm))
+            {
+                AlarmItems.Add(alarm);
+                AlarmItemsChanged?.Invoke(this, EventArgs.Empty);
+            }
         }
 
         public void Calibrate()
@@ -67,6 +84,14 @@ namespace HW03
     {
         private readonly DispatcherQueue _dispatcherQueue;
 
+        public ObservableCollection<TimeOnly> AlarmItems { get; set; }
+
+        [ObservableProperty]
+        public partial TimeSpan SelectedAlarmTime { get; set; } = TimeSpan.Zero;
+
+        [ObservableProperty]
+        public partial string AlarmText { get; set; } = "无事发生";
+
         public Clock DefaultClock => field;
 
         public TimeOnly DefaultClockTime => DefaultClock.ClockTime;
@@ -75,7 +100,11 @@ namespace HW03
         {
             DefaultClock = new Clock();
             DefaultClock.TimeChanged += OnTimeChanged;
+            DefaultClock.AlarmRaised += OnAlarmRaised;
+            DefaultClock.AlarmItemsChanged += OnAlarmItemsChanged;
             _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+
+            AlarmItems = new ObservableCollection<TimeOnly>();
         }
 
         [RelayCommand]
@@ -85,15 +114,43 @@ namespace HW03
             OnPropertyChanged(nameof(DefaultClockTime));
         }
 
+        [RelayCommand]
+        private void AddAlarm()
+        {
+            DefaultClock.AddAlarm(TimeOnly.FromTimeSpan(SelectedAlarmTime));
+        }
+
         // 在多线程环境中更新UI元素时，可能会遇到System.Runtime.InteropServices.COMException错误。
         // 这通常是因为UI元素只能在创建它们的线程（通常是主线程）上进行更新
         // 而Timer的回调方法是在一个线程池线程中执行的（非 UI 线程）
         // 使用 DispacherQueue 可以确保 OnPropertyChanged 在 UI 线程跟新
         private void OnTimeChanged(object? sender, EventArgs e)
         {
-            _dispatcherQueue.TryEnqueue(()=>
+            _dispatcherQueue.TryEnqueue(() =>
             {
+                // 通知 UI 更新
                 OnPropertyChanged(nameof(DefaultClockTime));
+            });
+        }
+
+        private void OnAlarmRaised(object? sender, EventArgs e)
+        {
+            _dispatcherQueue.TryEnqueue(() =>
+            {
+                AlarmText = "QAQ";
+            });
+        }
+
+        private void OnAlarmItemsChanged(object? sender, EventArgs e)
+        {
+            _dispatcherQueue.TryEnqueue(() =>
+            {
+                AlarmItems.Clear();
+                foreach (var alarm in DefaultClock.AlarmItems)
+                {
+                    AlarmItems.Add(alarm);
+                }
+                //OnPropertyChanged(nameof(AlarmItems));
             });
         }
     }
